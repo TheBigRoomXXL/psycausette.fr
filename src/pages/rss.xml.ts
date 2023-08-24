7f4c708126ed0a721f98b7366109f8d635b250fc
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-08-24 09:18:22 +0200

    refacto: update metadata

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { siteConfig } from "../site.config";
 
-export async function get(context) {
+export async function get(context: APIContext) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
+		title: siteConfig.title,
+		description: siteConfig.description,
+		site: context.site!,
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
---
+++ src/site.config.ts
@@ -0,0 +1,35 @@
+interface Link {
+    name: string;
+    url: string
+}
+
+interface SiteConfig {
+    author: string;
+    title: string;
+    description: string;
+    lang: string;
+    ogLocale: string;
+    themeColor: string;
+    menuLinks: Link[];
+    socialLinks: Link[];
+}
+
+export const siteConfig: SiteConfig = {
+    author: "Charlotte RAUSCHER",
+    title: "Psycho Kitty",
+    description: "Un blog de vulgarisation scientifique en psychologie",
+    lang: "fr-FR",
+    ogLocale: "fr_FR",
+    themeColor: "#A32CC4",
+    menuLinks: [
+        { name: "Home", url: "/" },
+        { name: "Blog", url: "/home" },
+        { name: "RSS", url: "/rss.xml" },
+
+    ],
+    socialLinks: [
+        // { name: "github", url: "https://github.com/TheBigRoomXXL" },
+        // { name: "itch.io", url: "https://tehbigroomxxl.itch.io/" },
+        // { name: "email", url: "mailto:seb.lovergne+site@gmail.com?Subject=Hello" },
+    ],
+};
