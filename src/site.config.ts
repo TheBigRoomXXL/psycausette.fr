033e57008003a277273f9704aadb799a5fd36b6a
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-08-24 09:36:39 +0200

    refacto: header and footer

--- src/site.config.ts
+++ src/site.config.ts
@@ -23,11 +23,11 @@ export const siteConfig: SiteConfig = {
     themeColor: "#A32CC4",
     menuLinks: [
         { name: "Home", url: "/" },
-        { name: "Blog", url: "/home" },
-        { name: "RSS", url: "/rss.xml" },
+        { name: "Blog", url: "/blog" },
 
     ],
     socialLinks: [
+        { name: "RSS", url: "/rss.xml" },
         // { name: "github", url: "https://github.com/TheBigRoomXXL" },
         // { name: "itch.io", url: "https://tehbigroomxxl.itch.io/" },
         // { name: "email", url: "mailto:seb.lovergne+site@gmail.com?Subject=Hello" },
